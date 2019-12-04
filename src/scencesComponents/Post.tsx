61dddf984084103488c76c80f81b8ed6417ea0f6
islamYMansour <islamy95@hotmail.com>
2019-12-04 10:28:52 +0200

    add post component

---
+++ src/scencesComponents/Post.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+export class Post extends Component {
+  render() {
+    return <View></View>;
+  }
+}
+
+export default Post;
